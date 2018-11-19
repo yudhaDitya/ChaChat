ef4bc3fbc3cc5f3af78d1eb53a8f915c0fe5470d
Yudha <yudha.ditya19@gmail.com>
2018-11-20 02:03:29 +0800

    [ADD] register function

--- chachat/LoginRegisterViewController.swift
+++ chachat/LoginRegisterViewController.swift
@@ -35,20 +35,11 @@ class LoginRegisterViewController: UIViewController {
     
     @IBAction func loginClicked(_ sender: Any) {
         print("login clicked")
-        if((emailTextField.text?.characters.count)! < 5)
-        {
-            emailTextField.backgroundColor = UIColor.init(red: 0.8, green: 0, blue: 0, alpha: 0.2)
+        
+        if(!CheckInput()){
             return
-        } else {
-            emailTextField.backgroundColor = UIColor.white
         }
-        if((passwordTextField.text?.characters.count)! < 5)
-        {
-            passwordTextField.backgroundColor = UIColor.init(red: 0.8, green: 0, blue: 0, alpha: 0.2)
-            return
-        } else {
-            passwordTextField.backgroundColor = UIColor.white
-        }
+        
         let email = emailTextField.text
         let password = passwordTextField.text
         
@@ -64,7 +55,57 @@ class LoginRegisterViewController: UIViewController {
     }
     
     @IBAction func registerClicked(_ sender: Any) {
+        print("register clicked")
+        if(!CheckInput()){
+            return
+        }
+        let alert = UIAlertController(title: "Register", message: "Please confirm password...", preferredStyle: .alert)
+        alert.addTextField {(textField) in
+            textField.placeholder = "password"
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {
+            (action) -> Void in
+            let passConfirm = alert.textFields![0] as UITextField
+            if (passConfirm.text!.isEqual(self.passwordTextField.text!)) {
+                
+                //reg begin
+                let email = self.emailTextField.text
+                let password = self.passwordTextField.text
+                
+                Auth.auth().createUser(withEmail: email!, password: password!, completion: {
+                    (user, error) in
+                    if let error = error {
+                        Utilities().showAlert(title: "error", message: error.localizedDescription, vc: self)
+                        return
+                    }
+                    self.dismiss(animated:true, completion: nil)
+                })
+                } else {
+                Utilities().showAlert(title: "Error", message: "Password not the same!", vc: self)
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
+    
+    func CheckInput () -> Bool {
+        if((emailTextField.text?.characters.count)! < 5)
+        {
+            emailTextField.backgroundColor = UIColor.init(red: 0.8, green: 0, blue: 0, alpha: 0.2)
+            return false
+        } else {
+            emailTextField.backgroundColor = UIColor.white
+        }
+        if((passwordTextField.text?.characters.count)! < 5)
+        {
+            passwordTextField.backgroundColor = UIColor.init(red: 0.8, green: 0, blue: 0, alpha: 0.2)
+            return false
+        } else {
+            passwordTextField.backgroundColor = UIColor.white
+        }
+        return true
+    }
+    
     @IBOutlet weak var forgotClicked: UIButton!
     /*
     // MARK: - Navigation
